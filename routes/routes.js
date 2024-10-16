2f9ecedcc96b7b098fdcc9c09094f40fd8bd1046
Martín Sarasola <martin.sarasola01@hotmail.com>
2024-10-16 17:52:17 -0300

    Added get products below stock on router.js

--- routes/routes.js
+++ routes/routes.js
@@ -49,4 +49,22 @@ router.get("/productos/categoria/:categoria", async (req, res) => {
   }
 });
 
+// Obtener PRODUCTOS con bajo stock
+router.get("/productos/bajo-stock/:cantidad", async (req, res) => {
+  try {
+    const products = await Products.find({
+      cantidad: { $lt: req.params.cantidad },
+    });
+
+    if (!products) {
+      return res
+        .status(404)
+        .send({ mensaje: "No se encontró el producto especificado" });
+    }
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send({ mensaje: "Hubo un error en el servidor", error });
+  }
+});
+
 module.exports = router;
