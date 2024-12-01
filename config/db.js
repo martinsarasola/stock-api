ab68c4de556cd36c37f061c84a3bcd7824a273e5
Martín Sarasola <martin.sarasola01@hotmail.com>
2024-12-01 15:33:59 -0300

    Added file vercel.json and changed files

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 const dbconnect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/dbStock");
+    await mongoose.connect(MONGO_URI);
     console.log("Conectado exitosamente a la base de datos");
   } catch (error) {
     console.log("Ha ocurrido un error: " + error);
